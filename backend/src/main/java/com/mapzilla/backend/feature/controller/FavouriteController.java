ff58cf3912fb99b8e8b9a284898acd80c1d84568
Michał Witkowski <mike.witkowski.dev@gmail.com>
2025-04-03 17:31:22 +0200

    project structure adjust

--- backend/src/main/java/com/mapzilla/backend/feature/controller/FavouriteController.java
+++ backend/src/main/java/com/mapzilla/backend/feature/controller/FavouriteController.java
@@ -1,4 +1,4 @@
-package com.mapzilla.backend.favourite.controller;
+package com.mapzilla.backend.feature.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
