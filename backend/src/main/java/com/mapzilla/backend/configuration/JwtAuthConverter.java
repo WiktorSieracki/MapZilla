2c05d8e6bf879bb0758724a98b91f212569d7275
Michał Witkowski <mike.witkowski.dev@gmail.com>
2025-04-03 17:28:13 +0200

    start work

--- backend/src/main/java/com/mapzilla/backend/configuration/JwtAuthConverter.java
+++ backend/src/main/java/com/mapzilla/backend/configuration/JwtAuthConverter.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
