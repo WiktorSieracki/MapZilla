e48fa60182e81a5fdf3a79e5e581b7caba38d8bd
emWitkowsky <m.witkowski.711@studms.ug.edu.pl>
2025-05-23 13:14:40 +0200

    remove unused files

--- backend/src/main/java/com/mapzilla/backend/feature/favouritePlace/repository/FavouritePlaceRepository.java
+++ backend/src/main/java/com/mapzilla/backend/feature/favouritePlace/repository/FavouritePlaceRepository.java
@@ -1,12 +1,9 @@
 package com.mapzilla.backend.feature.favouritePlace.repository;
 
 import com.mapzilla.backend.feature.favouritePlace.model.FavouritePlace;
-import com.mapzilla.backend.model.FavouriteLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface FavouritePlaceRepository extends JpaRepository<FavouritePlace, UUID> {
-    List<FavouritePlace> findByUserId(UUID userId);
 }
