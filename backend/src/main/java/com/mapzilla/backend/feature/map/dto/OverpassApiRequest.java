8b59cbbba2164df5c5f881d6ce493bf42fdd37f4
Bartlomiej Wnuk <bartlomiejwnuk03@gmail.com>
2025-06-05 17:33:43 +0200

    feat: add lombok validation for map locate around request

--- backend/src/main/java/com/mapzilla/backend/feature/map/dto/OverpassApiRequest.java
+++ backend/src/main/java/com/mapzilla/backend/feature/map/dto/OverpassApiRequest.java
@@ -1,6 +1,8 @@
 package com.mapzilla.backend.feature.map.dto;
 
 import com.mapzilla.backend.feature.map.enums.PlaceType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,19 @@ import java.util.List;
 @Getter
 @Setter
 public class OverpassApiRequest {
+
+    @NotNull(message = "Latitude is required")
+    @Min(value = 0, message = "Lat must be greater than 0")
     private double lat;
+
+    @NotNull(message = "Longitude is required")
+    @Min(value = 0, message = "Lon must be greater than 0")
     private double lon;
-    private int radius = 1200; // domyślny radius w metrach
+
+    @NotNull(message = "Longitude is required")
+    @Min(value = 0, message = "Lon must be greater than 0")
+    private int radius = 1200;
+
+    @NotNull(message = "List of notAvailablePlaces is required!")
     private List<PlaceType> types;
 }
